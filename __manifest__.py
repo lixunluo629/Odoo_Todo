d72805e5f6b0bd92f73665e1e80dcc61cd0ead22
Tomori <291340703@qq.com>
2019-09-04 20:36:08 +0800

    Upload

---
+++ __manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "todo",
+
+    'summary': """
+        待办事项列表""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/todo_security.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/menus.xml',
+        'security/todo_rule.xml',
+        'reports/todo_task_report.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ models/models.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+class TodoTask(models.Model):
+    _name = 'todo.task'
+    _description = '我的待办'
+
+    name = fields.Char(u'标题')
+    category_id = fields.Many2one('todo.category', string=u'分类')
+    is_done = fields.Boolean(u'已完成', default=False, track_visibility='onchange')
+    content = fields.Html(u'详细内容', strip_style=True)
+    progress_report = fields.Html(u'进度汇报', strip_style=True)
+    priority = fields.Selection([
+        ('normal', '普通'),
+        ('urgency', '紧急')
+    ], default='normal', required=True, string='紧急程度')
+    is_expired = fields.Boolean(u'已过期', compute='_compute_is_expired', readonly=True)
+    deadline = fields.Date(u'截止时间')
+    create_date = fields.Date(u'创建日期', required=True, readonly=True, index=True, default=fields.Date.today)
+    creator_id = fields.Many2one('res.users', string='安排人', default=lambda s: s.env.uid, required=True, readonly=True)
+    coordinator_id = fields.Many2one('res.users', string='协调人', default=lambda s: s.env.uid)
+    executor_id = fields.Many2one('res.users', string='执行人')
+    activate = fields.Boolean(u'已激活', required=True, readonly=True, index=True)
+    request_permit = fields.Boolean(u'申请审核', readonly=True, index=True,
+                                    track_visibility='onchange')
+    get_permit = fields.Boolean(u'审核通过', readonly=True, index=True, track_visibility='onchange')
+    WORKFLOW_STATE_SELECTION = [
+        ('draft', u'草稿'),
+        ('arrange', u'安排'),
+        ('done', u'完成'),
+        ('cancel', u'关闭')
+    ]
+    task_state = fields.Selection(WORKFLOW_STATE_SELECTION, string='状态', default='draft', readonly=True)
+    WORKFLOW_STATE_SELECTION2 = [
+        ('submit', u'执行中'),
+        ('review', u'审核中'),
+        ('permit', u'审核通过')
+    ]
+    request_state = fields.Selection(WORKFLOW_STATE_SELECTION2, string='完成情况', default='submit', readonly=True)
+    WORKFLOW_STATE_SELECTION3 = [
+        ('refuse', '拒绝申请'),
+        ('nopermit', '待审批'),
+        ('permitted', '审批通过'),
+    ]
+    permit_state = fields.Selection(WORKFLOW_STATE_SELECTION3, string='审批情况', default='nopermit', readonly=True)
+
+    @api.multi
+    def action_permit(self):
+        self.write({'request_state': 'permit'})
+        self.action_done()
+        for record in self:
+            record.request_permit = False
+            record.get_permit = True
+
+    @api.multi
+    def action_review(self):
+        self.write({'request_state': 'review'})
+        self.action_nopermit()
+        for record in self:
+            record.request_permit = True
+
+    @api.multi
+    def action_submit(self):
+        self.write({'request_state': 'submit'})
+        self.action_arrange()
+        for record in self:
+            record.request_permit = False
+
+    @api.multi
+    def action_draft(self):
+        self.write({'task_state': 'draft'})
+        for record in self:
+            record.activate = False
+
+    @api.multi
+    def action_arrange(self):
+        self.write({'task_state': 'arrange'})
+        for record in self:
+            record.activate = True
+
+    @api.multi
+    def action_done(self):
+        self.write({'task_state': 'done'})
+
+    @api.multi
+    def action_cancel(self):
+        self.write({'task_state': 'cancel'})
+
+    @api.multi
+    def action_nopermit(self):
+        self.write({'permit_state': 'nopermit'})
+
+    @api.multi
+    def action_permitted(self):
+        for record in self:
+            if record.request_permit:
+                self.write({'permit_state': 'permitted'})
+                self.action_permit()
+            else:
+                raise UserError('执行人还没提交完成呢, 错误项的标题为：%s' % self.name)
+                pass
+
+    @api.multi
+    def action_refuse(self):
+        self.write({'permit_state': 'refuse'})
+        self.action_submit()
+
+    @api.depends('deadline')
+    @api.multi
+    def _compute_is_expired(self):
+        for record in self:
+            if record.deadline:
+                record.is_expired = record.deadline < fields.Date.today()
+            else:
+                record.is_expired = False
+
+
+class TodoCategory(models.Model):
+    _name = 'todo.category'
+    _description = '分类列表'
+
+    name = fields.Char(u'分类名')
+    task_ids = fields.Many2one('todo.task', string='待办事项')
+    count = fields.Integer(u'任务数量', compute='_compute_task_count')
+
+    @api.depends('task_ids')
+    @api.multi
+    def _compute_task_count(self):
+        for record in self:
+            record.count = len(record.task_ids)
+
+
+class TodoSecurity(models.Model):
+    _inherit = 'ir.actions.server'
+
+    groups_id = fields.Many2many('res.groups', 'ir_act_server_group_rel', 'act_id', 'gid', string='权限群组')
+
+
+class TodoUserGroups(models.Model):
+    _inherit = 'res.groups'
